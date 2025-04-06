c7415fe3260360cf2ee25462a8a0edeb937141f1
Stuart MacCallum <maccalsa@googlemail.com>
2025-04-06 22:15:56 +0100

    Fix replacment script

--- name.sh
+++ name.sh
@@ -6,7 +6,10 @@ read -p "Enter your repository name: " repo_name
 echo "Your GitHub username is: $username"
 echo "Your repository name is: $repo_name"
 
-# Replace placeholders in all files
-find . -type f -exec sed -i "s/@<your-github-username>/<your-repo-name>/g" {} +
+script_name=$(basename "$0")
+# Replace placeholders in all files except name.sh
+find . -type f ! -name "$script_name" ! -name "*.sh" -exec sed -i "s/@<your-github-username>/@$username/g" {} +
+find . -type f ! -name "$script_name" ! -name "*.sh" -exec sed -i "s/<your-github-username>/$username/g" {} +
+find . -type f ! -name "$script_name" ! -name "*.sh" -exec sed -i "s/<your-repo-name>/@$repo_name/g" {} +
 
 echo "All placeholders have been replaced."
